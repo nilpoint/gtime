9dda31d8fc7b6d1b9f43571c37af7ebe3720dcc8
Risto Laanoja <ristik@gmail.com>
2013-07-31 15:32:37 +0300

    fix paths in .deb builder script

--- packaging/deb/create_deb.sh
+++ packaging/deb/create_deb.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+if [ ! -f ../../gtime-test ]; then
+	echo First compile the binary, i.e. go to ../.. and
+	echo "autoreconf -vi && ./configure --prefix=/usr && make"
+	exit -1
+fi
+
 mkdir tmp
 
 mkdir -p ./gtime/usr
@@ -6,16 +14,16 @@ mkdir -p ./gtime/usr/share/doc
 mkdir -p ./gtime/usr/share/doc/gtime
 mkdir -p ./gtime/usr/share/man/man1
 
-cp -f ../gtime-test gtime/usr/bin/gtime
-cp -f ../gtime-sign gtime/usr/bin/
-cp -f ../gtime-verify gtime/usr/bin/
-ln -f -s gtime/usr/bin/gtime gtime/usr/bin/gtime-test
-gzip -c ../doc/gtime.1 > gtime/usr/share/man/man1/gtime.1.gz
-gzip -c ../doc/gtime-test.1 > gtime/usr/share/man/man1/gtime-test.1.gz
-cp -f ../doc/trust.pem gtime/usr/share/doc/gtime/
-strip gtime*/usr/bin/gtime*
+cp -f ../../gtime-test gtime/usr/bin/gtime
+cp -f ../../gtime-sign gtime/usr/bin/
+cp -f ../../gtime-verify gtime/usr/bin/
+ln -f -s gtime gtime/usr/bin/gtime-test
+gzip -c ../../doc/gtime.1 > gtime/usr/share/man/man1/gtime.1.gz
+gzip -c ../../doc/gtime-test.1 > gtime/usr/share/man/man1/gtime-test.1.gz
+cp -f ../../doc/{trust.pem,run-tests.sh,tap-functions,TestData.txt,TestData.txt.gtts} gtime/usr/share/doc/gtime/
+strip gtime*/usr/bin/gtime
 
-VER="`grep ^AC_INIT ../configure.ac | sed -e 's/^[^0-9.-]*//' -e 's/\].*$//'`"
+VER="`grep ^AC_INIT ../../configure.ac | sed -e 's/^[^0-9.-]*//' -e 's/\].*$//'`"
 echo ver: $VER
 # REV=`svn info | grep Revision | sed "s/Revision:\s//i"`
 REV=`git describe`
