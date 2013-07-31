729bb9e1dbb3c3a88e499d894936d6ff1cfccc62
Risto Laanoja <ristik@gmail.com>
2013-07-31 10:03:27 +0300

    take care of blank directory structure

--- packaging/deb/create_deb.sh
+++ packaging/deb/create_deb.sh
@@ -1,9 +1,15 @@
 mkdir tmp
 
+mkdir -p ./gtime/usr
+mkdir -p ./gtime/usr/bin
+mkdir -p ./gtime/usr/share/doc
+mkdir -p ./gtime/usr/share/doc/gtime
+mkdir -p ./gtime/usr/share/man/man1
+
 cp -f ../gtime-test gtime/usr/bin/gtime
 cp -f ../gtime-sign gtime/usr/bin/
 cp -f ../gtime-verify gtime/usr/bin/
-ln -f -s gtime gtime/usr/bin/gtime-test
+ln -f -s gtime/usr/bin/gtime gtime/usr/bin/gtime-test
 gzip -c ../doc/gtime.1 > gtime/usr/share/man/man1/gtime.1.gz
 gzip -c ../doc/gtime-test.1 > gtime/usr/share/man/man1/gtime-test.1.gz
 cp -f ../doc/trust.pem gtime/usr/share/doc/gtime/
